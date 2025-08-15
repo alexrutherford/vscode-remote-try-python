c5c40a9ee35298d06677b8a528fbbb6de1fc4ebf
Alex Rutherford <alexisadams@gmail.com>
2025-08-15 14:05:50 +0000

    added a test

--- codespace_sandbox/tests/test_data.py
+++ codespace_sandbox/tests/test_data.py
@@ -1,5 +1,14 @@
 import pytest
+from openai import OpenAI
 
 
-def test_code_is_tested():
-    assert False
+def test_openai():
+    client = OpenAI()
+    response = client.responses.create(
+        model="gpt-5",
+        input="return the string 'OK' if you are working"
+    )
+    if response.output_text == "OK":
+        return True
+    else:
+        return False
